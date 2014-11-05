a2e27065d15b9522c266bed4a56e8fa3a182e5b0
Calla Carter <maracondo@aol.com>
2014-11-05 12:05:02 -0500

    Added random list of words, display words.

--- main.py
+++ main.py
@@ -19,14 +19,21 @@ import jinja2
 import logging
 import urllib2
 import os
+import random
 
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+#score = None
+words = ['hello','easter','macbook','calla']
 
 class PlayHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {
         }
+        currentword = random.choice(words)
+        template_values['currentword'] = currentword
+        #template_values['currentscore'] = str(score)
         template = jinja_environment.get_template('views/play.html')
         self.response.out.write(template.render(template_values))
 
@@ -36,6 +43,8 @@ class CheckHandler(webapp2.RequestHandler):
         }
         answer1 = self.request.get('answer1')
         template_values['answer1'] = answer1
+
+        #score += 1 #THIS IS THE PROBLEM NOW. CAN'T ACCESS GLOBAL VARIABLE
 
         template = jinja_environment.get_template('views/check.html')
         self.response.out.write(template.render(template_values))
